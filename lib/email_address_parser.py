0287af1b847035489b9b4c6e9a31cadfb35557b8
=SarahMumbua <mumbuasarah2030@gmail.com>
2023-05-29 18:02:04 +0300

    added email checker lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,9 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_string):
+        self.email_string = email_string
+
+    def parse(self):
+        email_list = re.findall(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b', self.email_string.replace(" ", ""))
+        return email_list
